4ae44bf65f8d43c99674092836fe04bef75043d9
Akash Sarda <akashsarda3@gmail.com>
2021-11-27 19:48:23 -0500

    checking remaining files

--- spec/models/course_participant_spec.rb
+++ spec/models/course_participant_spec.rb
@@ -3,13 +3,13 @@ describe "CourseParticipant" do
   let(:course) { build(:course, id: 1, name: 'ECE517')  }
   let(:assignment) { build(:assignment, id: 1, name: 'no assignment', participants: [participant], teams: [team])  } 
   let(:assignment_participant) {build(:participant, id: 1)}
-  '''describe "#copy" do
-    #before(:each) do
-    #  byebug
-    #  assignment = build(:assignment)
-    #  course_participant = build(:course_participant)
-    #  @assignment_participant = build(:participant)
-    #end
+  describe "#copy" do
+    before(:each) do
+     byebug
+     assignment = build(:assignment)
+     course_participant = build(:course_participant)
+     @assignment_participant = build(:participant)
+    end
 
     it "create a copy of participant" do
       allow(AssignmentParticipant).to receive(:create).and_return(participant)
@@ -24,53 +24,55 @@ describe "CourseParticipant" do
 
       expect(course_participant.copy(assignment.id)).to be_nil
     end
-  end'''
+  end
 
   describe ".import" do
-    context 'when record is empty' do
-      it 'raises an ArgumentError' do
-        byebug
-        expect { CourseParticipant.import({}, nil, nil) }.to raise_error(ArgumentError)
-      end
-    end
-
-    context 'when the record does not have required items' do
-      it 'raises an ArgumentError' do
-        row = {name: 'no one', fullname: 'no one'}
-        expect { CourseParticipant.import(row, nil, 1) }.to raise_error(ArgumentError)
-      end
-    end
-
-    # context 'when no user is found by provided username' do
-    #   context 'when the record has required items' do
-    #     let(:row) do
-    #       {name: 'no one', fullname: 'no one', email: 'name@email.com'}
-    #     end
-    #     before(:each) do
-    #       user = double("User", :id => 1, :nil? => true)
-    #       allow(User).to receive(:find_by).with(:name => "no one").and_return(user)
-    #       allow(User).to receive(:import).with(any_args).and_return(user)
-    #     end
-
-    #     context 'when course cannot be found' do
-    #       it 'creates a new user then raises an ImportError' do
-    #         allow(Course).to receive(:find_by).with(1).and_return(nil)
-    #         expect(User).to receive(:import).with(any_args)
-    #         expect { CourseParticipant.import(row, nil, 1) }.to raise_error(ImportError, 'The course with id 1 was not found.')
-    #       end
-    #     end
-
-    #     context 'when course found and course participant does not exist' do
-    #       it 'creates a new user and participant' do
-    #         allow(Course).to receive(:find_by).with(1).and_return(course)
-    #         allow(CourseParticipant).to receive(:exists?).with(user_id: 1, parent_id: 1).and_return(false)
-    #         expect(User).to receive(:import).with(any_args)
-    #         expect(CourseParticipant).to receive(:create).with(user_id: 1, parent_id: 1)
-    #         CourseParticipant.import(row, nil, 1)
-    #       end
-    #     end
+    # context 'when record is empty' do
+    #   it 'raises an ArgumentError' do
+    #     byebug
+    #     expect { CourseParticipant.import({}, nil, nil) }.to raise_error(ArgumentError)
     #   end
     # end
+
+    # context 'when the record does not have required items' do
+    #   it 'raises an ArgumentError' do
+    #     row = {name: 'no one', fullname: 'no one'}
+    #     expect { CourseParticipant.import(row, nil, 1) }.to raise_error(ArgumentError)
+    #   end
+    # end
+
+    context 'when no user is found by provided username' do
+      context 'when the record has required items' do
+        let(:row) do
+          {name: 'no one', fullname: 'no one', email: 'name@email.com'}
+        end
+        before(:each) do
+          user = double("User", :id => 1, :nil? => true)
+          allow(User).to receive(:find_by).with(:name => "no one").and_return(user)
+          allow(User).to receive(:import).with(any_args).and_return(user)
+        end
+
+        context 'when course cannot be found' do
+          it 'creates a new user then raises an ImportError' do
+            byebug
+            allow(Course).to receive(:find_by).with(1).and_return(nil)
+            expect(User).to receive(:import).with(any_args)
+            expect { CourseParticipant.import(row, nil, 1) }.to raise_error(ImportError, 'The course with id 1 was not found.')
+          end
+        end
+
+        context 'when course found and course participant does not exist' do
+          it 'creates a new user and participant' do
+            byebug
+            allow(Course).to receive(:find_by).with(1).and_return(course)
+            allow(CourseParticipant).to receive(:exists?).with(user_id: 1, parent_id: 1).and_return(false)
+            expect(User).to receive(:import).with(any_args)
+            expect(CourseParticipant).to receive(:create).with(user_id: 1, parent_id: 1)
+            CourseParticipant.import(row, nil, 1)
+          end
+        end
+      end
+    end
   end
 
   # describe "#export" do
